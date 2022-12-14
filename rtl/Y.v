ad6183b4c86f42e7e6f9e36af2f77e512b6e3f02
Gabor Sebastian <sebi.gabor20@outlook.com>
2022-12-14 12:18:30 +0200

    Added registers

---
+++ rtl/Y.v
@@ -0,0 +1,16 @@
+module Y(
+  input [15:0] in,
+  input clk,rst,w,
+  output reg [15:0] out);
+
+  always @(negedge clk, negedge rst)
+  begin
+     if (!rst) begin
+        out <= 16'd0;
+     end
+   else if (w) begin
+        out <= in;
+   end
+  end
+
+endmodule
